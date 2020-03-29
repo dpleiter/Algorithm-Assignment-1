17e60de06e7f2550736e000dadac95e7588d47d0
dpleiter <dpleiter@gmail.com>
2020-03-29 13:42:29 +1100

    Fixed adding of duplicates

--- BinarySearchTreeRQ.java
+++ BinarySearchTreeRQ.java
@@ -10,6 +10,71 @@ import java.lang.String;
  * @author Sajal Halder, Minyi Li, Jeffrey Chan
  */
 public class BinarySearchTreeRQ implements Runqueue {
+    Node head;
+
+    public BinarySearchTreeRQ() {
+        this.head = null;
+    } // end of BinarySearchTreeRQ()
+
+    @Override
+    public void enqueue(String procLabel, int vt) {
+        Node nodeToAdd = new Node(procLabel, vt);
+
+        if (head == null) {
+            this.head = nodeToAdd;
+        } else {
+            this.head.addChild(nodeToAdd);
+        }
+    }
+
+    @Override
+    public String dequeue() {
+        Node nodeToDequeue = head.findMinimumVt();
+
+        if (nodeToDequeue == this.head) {
+            // dequeue existing head and set new head as right child
+            this.head = this.head.getRight();
+            this.head.setParent(null);
+        } else {
+            nodeToDequeue.getParent().setLeft(nodeToDequeue.getRight());
+        }
+
+        return nodeToDequeue.getProcLabel();
+    } // end of dequeue()
+
+    @Override
+    public boolean findProcess(String procLabel) {
+        return this.head.findProcLabel(procLabel);
+    } // end of findProcess()
+
+    @Override
+    public boolean removeProcess(String procLabel) {
+        // Implement me
+
+        return false; // placeholder, modify this
+    } // end of removeProcess()
+
+    @Override
+    public int precedingProcessTime(String procLabel) {
+        // Implement me
+
+        return -1; // placeholder, modify this
+    } // end of precedingProcessTime()
+
+    @Override
+    public int succeedingProcessTime(String procLabel) {
+        // Implement me
+
+        return -1; // placeholder, modify this
+    } // end of precedingProcessTime()
+
+    @Override
+    public void printAllProcesses(PrintWriter os) {
+        this.head.printTree();
+
+        System.out.print("\n");
+    }
+
     @SuppressWarnings("unused")
     private class Node {
         String procLabel;
@@ -29,22 +94,22 @@ public class BinarySearchTreeRQ implements Runqueue {
         }
 
         public void addChild(Node nodeToAdd) {
-            if (nodeToAdd.getVt() > this.vt) {
-                if (this.parent != null && nodeToAdd.getVt() == this.parent.getVt()) {
-                    // Insert duplicate key
-                    nodeToAdd.setParent(this.parent);
-                    nodeToAdd.setLeft(this);
-
-                    this.parent.setLeft(nodeToAdd);
-                    this.parent = nodeToAdd;
-                } else if (this.right == null) {
+            if (nodeToAdd.getVt() >= this.vt) {
+                if (this.right == null) {
                     this.right = nodeToAdd;
                     nodeToAdd.setParent(this);
                 } else {
                     this.right.addChild(nodeToAdd);
                 }
             } else {
-                if (this.left == null) {
+                if (this.parent != null && this.parent.getVt() == nodeToAdd.getVt()) {
+                    // Add duplicate key here
+                    nodeToAdd.setParent(this.parent);
+                    nodeToAdd.setRight(this);
+
+                    this.parent.setRight(nodeToAdd);
+                    this.setParent(nodeToAdd);
+                } else if (this.left == null) {
                     this.left = nodeToAdd;
                     nodeToAdd.setParent(this);
                 } else {
@@ -81,12 +146,16 @@ public class BinarySearchTreeRQ implements Runqueue {
             this.left = node;
         }
 
+        public void setRight(Node node) {
+            this.right = node;
+        }
+
         public void printTree() {
             if (this.left != null) {
                 this.left.printTree();
             }
 
-            System.out.print(this.procLabel + " ");
+            System.out.print(this.procLabel + "=" + Integer.toString(this.vt) + " ");
 
             if (this.right != null) {
                 this.right.printTree();
@@ -94,6 +163,7 @@ public class BinarySearchTreeRQ implements Runqueue {
         }
 
         public Node findMinimumVt() {
+            // finds minimum vt in tree and returns that node
             if (this.left == null) {
                 return this;
             } else {
@@ -109,74 +179,25 @@ public class BinarySearchTreeRQ implements Runqueue {
                         || (this.right != null && this.right.findProcLabel(procLabel));
             }
         }
-    }
 
-    Node head;
+        public void printDetails() {
+            // For testing
+            System.out.println("\nDetails of node " + procLabel);
 
-    /**
-     * Constructs empty queue
-     */
-    public BinarySearchTreeRQ() {
-        this.head = null;
-    } // end of BinarySearchTreeRQ()
+            if (this.parent == null) {
+                System.out.println("HEAD NODE");
+            } else {
+                System.out.println("Parent: " + parent.getProcLabel());
+            }
 
-    @Override
-    public void enqueue(String procLabel, int vt) {
-        Node nodeToAdd = new Node(procLabel, vt);
+            if (left != null) {
+                System.out.println("Left Child: " + left.getProcLabel());
+            }
 
-        if (head == null) {
-            this.head = nodeToAdd;
-        } else {
-            this.head.addChild(nodeToAdd);
+            if (right != null) {
+                System.out.println("Right Child: " + right.getProcLabel());
+            }
         }
-    }
-
-    @Override
-    public String dequeue() {
-        Node nodeToDequeue = head.findMinimumVt();
-
-        if (nodeToDequeue == this.head) {
-            // dequeue the head
-            // as no nodes to left, set head as the right node
-            this.head = this.head.getRight();
-        } else {
-            nodeToDequeue.getParent().setLeft(null);
-        }
-
-        return nodeToDequeue.getProcLabel();
-    } // end of dequeue()
-
-    @Override
-    public boolean findProcess(String procLabel) {
-        return this.head.findProcLabel(procLabel);
-    } // end of findProcess()
-
-    @Override
-    public boolean removeProcess(String procLabel) {
-        // Implement me
-
-        return false; // placeholder, modify this
-    } // end of removeProcess()
-
-    @Override
-    public int precedingProcessTime(String procLabel) {
-        // Implement me
-
-        return -1; // placeholder, modify this
-    } // end of precedingProcessTime()
-
-    @Override
-    public int succeedingProcessTime(String procLabel) {
-        // Implement me
-
-        return -1; // placeholder, modify this
-    } // end of precedingProcessTime()
-
-    @Override
-    public void printAllProcesses(PrintWriter os) {
-        head.printTree();
-
-        System.out.print("\n");
-    }
+    } // end of class Node
 
 } // end of class BinarySearchTreeRQ
