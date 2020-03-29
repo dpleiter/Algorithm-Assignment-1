826c568c2f87b7915c23ddab0e9be7cc51e680dd
Oliver Eaton <oliverf.eaton@gmail.com>
2020-03-29 17:54:10 +1100

    Enqueue complete on array

--- OrderedArrayRQ.java
+++ OrderedArrayRQ.java
@@ -25,15 +25,49 @@ public class OrderedArrayRQ implements Runqueue {
 
     @Override
     public void enqueue(String procLabel, int vt) {
+    	
         // Implement me
-        int new_array_length = array.length + 1;
+    	int new_array_length = 0;
+    	if (array == null)
+    		new_array_length = 1;
+    	else 
+    		new_array_length = array.length + 1;
         String temp_array[] = new String[new_array_length];
-        String enqueued_node = procLabel + "," + String.valueOf(vt); // could use Integer.toString() here
-
-        temp_array[0] = enqueued_node;
-        for(int i=1; i < new_array_length; i++) {
-            temp_array[i] = array[i];
+        String enqueued_node = procLabel + "," + Integer.toString(vt);
+        
+        if(new_array_length == 1) {
+        	temp_array[0] = enqueued_node;
+        } else {
+        	
+            boolean node_included = false;
+            for(int i=0; i < array.length; i++) {
+            	
+            	while(node_included == false) { 
+            		
+	            	if(vt < Integer.parseInt(array[i].split(",")[1].trim())){
+	                	temp_array[i] = enqueued_node;
+	                    node_included = true;
+	                
+	            	} else if(vt > Integer.parseInt(array[i].split(",")[1].trim())) {	
+	                	temp_array[i] = array[i];
+	                	break;
+	                
+	            	} else {
+	                	temp_array[i+1] = array[i];
+	                
+	            	}
+	            }
+	            
+	            if(node_included == false)
+	            	temp_array[new_array_length-1] = enqueued_node;
+	            else {
+	            	for(int j=i+1; j < new_array_length; j++) {
+	            		temp_array[j] = array[j-1];
+	            	}
+	            }
+            }
         }
+
         array = temp_array;
 
     } // end of enqueue()
@@ -42,19 +76,21 @@ public class OrderedArrayRQ implements Runqueue {
     @Override
     public String dequeue() throws NullPointerException {
         // Implement me
-        if(array==null) {
-    		throw new NullPointerException("The queue is already empty.");
-    	} else {
+        if(array==null)
+    		throw new NullPointerException("Nothing to dequeue.");
+  
+        String dequeued_node = array[0];
+        int new_array_length = array.length - 1;
+        String temp_array[] = new String[new_array_length];
 
-            String dequeued_node = array[array.length];
-            int new_array_length = array.length - 1
-            String temp_array[] = new String[new_array_length];
-
-        	for(int i=0; i < new_array_length; i++) {
-        		temp_array[i] = array[i];
-        	}
-        	array = temp_array;
+    	for(int i=1; i < array.length; i++) {
+    		temp_array[i-1] = array[i];
     	}
+    	
+    	if(new_array_length == 0)
+    		array = null;
+    	else
+    		array = temp_array;
 
         return dequeued_node + "; has left the queue."; // placeholder,modify this
     } // end of dequeue()
