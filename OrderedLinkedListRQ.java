a76c28be8700668ef586b74b6cc34275e262320a
dpleiter <dpleiter@gmail.com>
2020-04-05 20:29:12 +1000

    Added recursion into Linked List

--- OrderedLinkedListRQ.java
+++ OrderedLinkedListRQ.java
@@ -2,259 +2,211 @@ import java.io.PrintWriter;
 
 import java.lang.String;
 
-
-
 /**
  * Implementation of the run queue interface using an Ordered Link List.
  *
- * Your task is to complete the implementation of this class.
- * You may add methods and attributes, but ensure your modified class compiles and runs.
+ * Your task is to complete the implementation of this class. You may add
+ * methods and attributes, but ensure your modified class compiles and runs.
  *
  * @author Sajal Halder, Minyi Li, Jeffrey Chan.
  */
 public class OrderedLinkedListRQ implements Runqueue {
-	
-    /** Reference to head node. */
-    protected Proc mHead;
-    
-    /** Reference to tail of list. */
-    protected Proc mTail;
-    
-    /** Length of list. */
-    protected int mLength;
 
-    
-    /**
-     * Constructs empty linked list
-     */
-    public OrderedLinkedListRQ() {
-        mHead = null;
-        mTail = null;
-        mLength = 0;
-    }  // end of OrderedLinkedList()
+	/** Reference to head node. */
+	private Proc mHead;
 
+	/** Reference to tail of list. */
+	private Proc mTail;
 
-    @Override
-    public void enqueue(String procLabel, int vt) {
-    	
-        Proc newProc = new Proc(procLabel, vt);
+	/** Length of list. */
 
-        // If head is empty, then list is empty and head reference need to be initialised.
-        if (mLength == 0) {
-            mHead = newProc;
-            mTail = newProc;
-        } 
-        // otherwise distinguish where Proc needs to be placed, and insert it there.
-        else {
-        	int insertIndex = getIndex(vt);
+	/**
+	 * Constructs empty linked list
+	 */
+	public OrderedLinkedListRQ() {
+		this.mHead = null;
+		this.mTail = null;
+	} // end of OrderedLinkedList()
 
-            // if index = 0, we should replace mHead with newNode
-            if (insertIndex == 0) {
-            	newProc.setNext(mHead);
-            	newProc.mNext.setPrev(newProc);
-                mHead = newProc;
-            } else if(insertIndex == mLength) {
-            	newProc.setPrev(mTail);
-            	newProc.mPrev.setNext(newProc);
-            	mTail = newProc;
-            } else {
-                Proc currProc = mHead;
-                for (int i = 0; i < insertIndex-1; ++i) {
-                	currProc = currProc.getNext();
-                }
+	@Override
+	public void enqueue(String procLabel, int vt) {
+		Proc newProc = new Proc(procLabel, vt);
 
-                newProc.setNext(currProc.mNext);
-                newProc.setPrev(currProc);
-                currProc.setNext(newProc);
-                newProc.mNext.setPrev(newProc);
-            }
-        }
+		if (this.mHead == null) {
+			// First item in list
+			this.mHead = newProc;
+			this.mTail = newProc;
+		} else if (newProc.getRunTime() < this.mHead.getRunTime()) {
+			// Set new head
+			newProc.setNext(this.mHead);
+			this.mHead.setPrev(newProc);
 
-        mLength++;
+			this.mHead = newProc;
+		} else if (newProc.getRunTime() >= this.mTail.getRunTime()) {
+			// Set new tail
+			newProc.setPrev(this.mTail);
+			this.mTail.setNext(newProc);
 
-    } // end of enqueue()
-    
-    
+			this.mTail = newProc;
+		} else {
+			// Insert somewhere between head and tail
+			this.mHead.addProc(newProc);
+		}
+	} // end of enqueue()
 
+	@Override
+	public String dequeue() {
+		Proc procToDequeue = this.mHead;
 
-    @Override
-    public String dequeue() {
-    	
-    	String dequeuedProcess = mHead.getProcLabel();
-    	if(mLength == 1) {
-    		mHead = null;
-    		mTail = null;
-    	} else {
-        	Proc currProc = mHead.mNext;
-	    	currProc.setPrev(null);
-	    	mHead = currProc;
-    	}
-    	
-    	mLength--;
-    	
-        return dequeuedProcess;
-    } // end of dequeue()
+		if (this.mHead == this.mTail) {
+			// Last item in list
+			this.mHead = null;
+			this.mTail = null;
+		} else {
+			this.mHead = this.mHead.getNext();
+			this.mHead.setPrev(null);
+		}
 
+		return procToDequeue.getProcLabel();
+	} // end of dequeue()
 
-    @Override
-    public boolean findProcess(String procLabel) {
-        // Implement me
-    	boolean result = false;
-    	Proc currProc = mHead;
-    	
-    	while(currProc != null) {
-	    	if(currProc.getProcLabel().equalsIgnoreCase(procLabel)) {
-	    		result = true;
-	    		break;
-	    	} else
-	    		currProc = currProc.mNext;
-    	}
-        return result;
-    } // end of findProcess()
+	@Override
+	public boolean findProcess(String procLabel) {
+		return this.mHead.getProcByLabel(procLabel) != null;
+	} // end of findProcess()
 
+	@Override
+	public boolean removeProcess(String procLabel) {
+		Proc procToRemove = this.mHead.getProcByLabel(procLabel);
 
-    @Override
-    public boolean removeProcess(String procLabel) {
-        boolean processRemoved = false;
-        
-        Integer removeIndex = getIndex(procLabel);
-        if(removeIndex != null) {
-        	
-        	Proc currProc = mHead;
-            for (int i = 0; i < removeIndex; i++) {
-            	currProc = currProc.getNext();
-            }
-            
-            if(removeIndex == 0 && mLength == 1) {
-            	mHead = null;
-            	mTail = null;
-            } else if(removeIndex == 0 && mLength == 2) {
-            	mHead = currProc.mNext;
-            	mTail = currProc.mNext;
-            	currProc.mNext.setPrev(null);
-            } else if(removeIndex == 1 && mLength == 2) {
-            	mHead = currProc.mPrev;
-            	mTail = currProc.mPrev;
-            	currProc.mPrev.setNext(null);
-            } else if(removeIndex == 0 && mLength > 2) {
-            	mHead = currProc.mNext;
-            	currProc.mNext.setPrev(null);
-            } else if(removeIndex == mLength-1 && mLength > 2) {
-            	mTail = currProc.mPrev;
-            	currProc.mPrev.setNext(null);
-            } else {
-            	currProc.mPrev.setNext(currProc.mNext);
-            	currProc.mNext.setPrev(currProc.mPrev);
-            }
-            
-        	processRemoved = true;
-        	mLength--;
-        	
-        } 
-        
-        return processRemoved;
-    } // End of removeProcess()
+		if (procToRemove == null) {
+			return false;
+		} else if (this.mHead == this.mTail) {
+			// Last item in list
+			this.mHead = null;
+			this.mTail = null;
+		} else if (procToRemove == this.mHead) {
+			this.mHead = this.mHead.getNext();
+			this.mHead.setPrev(null);
+		} else if (procToRemove == this.mTail) {
+			this.mTail = this.mTail.getPrev();
+			this.mTail.setNext(null);
+		} else {
+			procToRemove.getPrev().setNext(procToRemove.getNext());
+			procToRemove.getNext().setPrev(procToRemove.getPrev());
+		}
 
-    
-    @Override
-    public int precedingProcessTime(String procLabel) {
-        
-    	int time = -1;
-        Integer index = getIndex(procLabel);
-        if(index != null) {
-        	time = 0;
-        	Proc currProc = mHead;
-            for (int i = 0; i < index; i++) {
-            	time = time + currProc.getRunTime();
-            	currProc = currProc.getNext();
-            }
+		return true;
+	} // End of removeProcess()
 
-        }
+	@Override
+	public int precedingProcessTime(String procLabel) {
+		Proc proc = this.mHead.getProcByLabel(procLabel);
 
-        return time;
-    } // end of precedingProcessTime()
+		return proc == null ? -1 : (proc.calcPreceedingTime() - proc.getRunTime());
+	} // end of precedingProcessTime()
 
+	@Override
+	public int succeedingProcessTime(String procLabel) {
+		Proc proc = this.mHead.getProcByLabel(procLabel);
 
-    @Override
-    public int succeedingProcessTime(String procLabel) {
-    	
-    	int time = -1;
-        Integer index = getIndex(procLabel);
-        if(index != null) {
-        	time = 0;
-        	Proc currProc = mTail;
-            for (int i = mLength-1; i > index; i--) {
-            	time = time + currProc.getRunTime();
-            	currProc = currProc.getPrev();
-            }
-        }
+		return proc == null ? -1 : (proc.calcSucceedingTime() - proc.getRunTime());
+	} // end of precedingProcessTime()
 
-        return time;
-    } // end of precedingProcessTime()
+	@Override
+	public void printAllProcesses(PrintWriter os) {
+		String processes = "";
+		Proc currProc = this.mHead;
 
+		while (currProc != null) {
+			processes = processes.concat(currProc.getProcLabel() + " ");
+			currProc = currProc.getNext();
+		}
 
-    @Override
-    public void printAllProcesses(PrintWriter os) {
-        
-    	String processes = "";
-    	Proc currProc = mHead;
-    	for(int i = 0; i < mLength; i++) {
-    		processes = processes.concat(currProc.getProcLabel() + " ");
-    		currProc = currProc.getNext();
-    	}
-    	os.println(processes.trim());
-    	
-    } // end of printAllProcess()
-    
-    
-    public Integer getIndex(int vt) {
-    	
-    	int index = 0;
-    	int i = mLength;
-    	
-    	Proc currProc = mTail;
-    	if(mLength > 1) {
-    		while(currProc != null) {   			
-		    	if(vt >= currProc.getRunTime()) {
-		    		index = i;
-		    		break;
-		    	} else {
-		    		currProc = currProc.getPrev();
-		    		i--;
-		    	}			
-	    	} 	
-    	} else {
-    		if(vt >= currProc.getRunTime())
-    			index = 1;
-    	}
-    	return index;
-    	
-    }
-    
-    
-    public Integer getIndex(String procLabel) {
-    	
-    	Integer index = null;
-    	int i = mLength;
-    	
-    	Proc currProc = mTail;
-    	if(mLength > 1) {
-    		while(currProc != null) { 
-    			i--;
-		    	if(procLabel.equalsIgnoreCase(currProc.getProcLabel())) {
-		    		index = i;
-		    		break;
-		    	} else {
-		    		currProc = currProc.getPrev();
-		    	}			
-	    	} 	
-    	} else {
-    		if(procLabel.equalsIgnoreCase(mHead.getProcLabel()))
-    			index = 0;
-    	}
-    	
-    	return index;
-    	
-    }
+		os.println(processes.trim());
+	} // end of printAllProcess()
 
+	private class Proc {
+
+		/** Process Label. */
+		protected String mProcLabel;
+
+		/** Process RunTime. */
+		private int mVt;
+
+		/** Reference to next node. */
+		private Proc mNext;
+
+		/** Reference to previous node. */
+		private Proc mPrev;
+
+		public Proc(String procLabel, int vt) {
+			mProcLabel = procLabel;
+			mVt = vt;
+			mNext = null;
+			mPrev = null;
+		}
+
+		public void addProc(Proc newProc) {
+			// Find position to add node using recursion
+			if (newProc.getRunTime() < this.mVt) {
+				newProc.setPrev(this.mPrev);
+				newProc.getPrev().setNext(newProc);
+
+				newProc.setNext(this);
+				this.mPrev = newProc;
+			} else {
+				this.mNext.addProc(newProc);
+			}
+		}
+
+		public int calcPreceedingTime() {
+			if (this.mPrev == null) {
+				return this.mVt;
+			} else {
+				return this.mPrev.calcPreceedingTime() + this.mVt;
+			}
+		}
+
+		public int calcSucceedingTime() {
+			if (this.mNext == null) {
+				return this.mVt;
+			} else {
+				return this.mNext.calcSucceedingTime() + this.mVt;
+			}
+		}
+
+		// Helper function
+		public Proc getProcByLabel(String procToFind) {
+			if (this.mProcLabel.compareTo(procToFind) == 0) {
+				return this;
+			} else {
+				return mNext == null ? null : mNext.getProcByLabel(procToFind);
+			}
+		}
+
+		public String getProcLabel() {
+			return mProcLabel;
+		}
+
+		public int getRunTime() {
+			return mVt;
+		}
+
+		public Proc getNext() {
+			return mNext;
+		}
+
+		public Proc getPrev() {
+			return mPrev;
+		}
+
+		public void setNext(Proc next) {
+			mNext = next;
+		}
+
+		public void setPrev(Proc prev) {
+			mPrev = prev;
+		}
+	}
 } // end of class OrderedLinkedListRQ
