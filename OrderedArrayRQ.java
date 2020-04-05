26ab7ebef352584f98b9a6416ac79f1845d93473
dpleiter <dpleiter@gmail.com>
2020-04-05 18:57:40 +1000

    cleanup of OrderedArray

--- OrderedArrayRQ.java
+++ OrderedArrayRQ.java
@@ -1,209 +1,193 @@
 import java.io.PrintWriter;
 import java.lang.String;
 
-
 /**
  * Implementation of the Runqueue interface using an Ordered Array.
  *
- * Your task is to complete the implementation of this class.
- * You may add methods and attributes, but ensure your modified class compiles and runs.
+ * Your task is to complete the implementation of this class. You may add
+ * methods and attributes, but ensure your modified class compiles and runs.
  *
  * @author Sajal Halder, Minyi Li, Jeffrey Chan
  */
 public class OrderedArrayRQ implements Runqueue {
+	private Proc array[];
 
-    protected Proc array[];
+	/**
+	 * Constructs empty queue
+	 */
+	public OrderedArrayRQ() {
+		array = null;
+	} // end of OrderedArrayRQ()
 
-    /**
-     * Constructs empty queue
-     */
-    public OrderedArrayRQ() {
-        // Implement Me
-        array = null;
-    }  // end of OrderedArrayRQ()
+	@Override
+	public void enqueue(String procLabel, int vt) {
+		Proc enqueuedProc = new Proc(procLabel, vt);
 
+		if (array == null) {
+			array = new Proc[] { enqueuedProc };
+		} else {
+			Proc[] tempArray = new Proc[array.length + 1];
 
-    @Override
-    public void enqueue(String procLabel, int vt) {
-    	
-    	Proc enqueuedProc = new Proc(procLabel, vt);
-     	int newArrayLength;
-    	if (array == null)
-    		newArrayLength = 1;
-    	else 
-    		newArrayLength = array.length + 1;
-        Proc tempArray[] = new Proc[newArrayLength];
-        
-        if(newArrayLength == 1) {
-        	tempArray[0] = enqueuedProc;
-        } else {
-        	
-        	Integer position = getPosition(vt);
-        	
-        	for(int i = 0; i < position; i++) {
-        		tempArray[i] = array[i];
-        	}
-        	
-        	tempArray[position] = enqueuedProc;
-        	
-        	for(int i = position+1; i < newArrayLength; i++) {
-        		tempArray[i] = array[i-1];
-        	}
-        }
+			Integer position = getPosition(vt);
 
-        array = tempArray;
-    } // end of enqueue()
+			for (int i = 0; i < position; i++) {
+				tempArray[i] = array[i];
+			}
 
+			tempArray[position] = enqueuedProc;
 
-    @Override
-    public String dequeue() throws NullPointerException {
-        // Implement me
-        if(array==null)
-    		throw new NullPointerException("Nothing to dequeue.");
-  
-        Proc dequeuedProc = array[0];
-        int newArrayLength = array.length - 1;
-        Proc tempArray[] = new Proc[newArrayLength];
+			for (int i = position + 1; i < tempArray.length; i++) {
+				tempArray[i] = array[i - 1];
+			}
 
-    	for(int i=1; i < array.length; i++) {
-    		tempArray[i-1] = array[i];
-    	}
-    	
-    	if(newArrayLength == 0)
-    		array = null;
-    	else
-    		array = tempArray;
+			array = tempArray;
+		}
+	} // end of enqueue()
 
-        return dequeuedProc.getProcLabel() + " has left the queue.";
-    } // end of dequeue()
+	@Override
+	public String dequeue() throws NullPointerException {
+		// Implement me
+		if (array == null) {
+			throw new NullPointerException("Nothing to dequeue.");
+		}
 
+		Proc dequeuedProc = array[0];
+		int newArrayLength = array.length - 1;
+		Proc tempArray[] = new Proc[newArrayLength];
 
-    @Override
-    public boolean findProcess(String procLabel){
-        // Implement me
-    	boolean found;
-    	Integer position = getPosition(procLabel);
-    	
-    	if(position == null)
-    		found = false;
-    	else
-    		found = true;
-    	
-    	return found;
-    } // end of findProcess()
+		for (int i = 1; i < array.length; i++) {
+			tempArray[i - 1] = array[i];
+		}
 
+		if (newArrayLength == 0)
+			array = null;
+		else
+			array = tempArray;
 
-    @Override
-    public boolean removeProcess(String procLabel) throws NullPointerException {
+		return dequeuedProc.getProcLabel();
+	} // end of dequeue()
 
-    	if(array == null)
-    		throw new NullPointerException("Queue is empty");
-    	
-    	boolean removedProc = false;
-        Integer position = getPosition(procLabel);
-        int newArrayLength = array.length - 1;
-		Proc tempArray[] = new Proc[newArrayLength];    	
-		
-    	if(position == null) {
-    		removedProc = false;
-    	} else {    		
-    		for(int i = 0; i < position; i++) {
-    			tempArray[i] = array[i];
-    		}
-    		for(int j = position+1; j < array.length; j++) {
-    			tempArray[j-1] = array[j];
-    		}
-    		
-    		if(newArrayLength == 0)
-    			array = null;
-    		else
-    			array = tempArray;
-    		
-    		removedProc = true;
-    	}
-    	
-    	return removedProc;
-    } // end of removeProcess()
+	@Override
+	public boolean findProcess(String procLabel) {
+		// Implement me
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].getProcLabel().compareTo(procLabel) == 0) {
+				return true;
+			}
+		}
 
+		return false;
+	} // end of findProcess()
 
-    @Override
-    public int precedingProcessTime(String procLabel) {
- 
-    	int preceedingRunTime = 0;
-        Integer position = this.getPosition(procLabel);
-    	
-    	if(position != null) {    		
-	    	for(int i=0; i < position; i++) {
-	    		Proc currProc = array[i];
-	    		preceedingRunTime = preceedingRunTime + currProc.getRunTime();
-	    	}
-    	} else
-    		preceedingRunTime = -1;
-    		
-    	return preceedingRunTime;
-    }// end of precedingProcessTime()
+	@Override
+	public boolean removeProcess(String procLabel) throws NullPointerException {
+		if (array == null)
+			throw new NullPointerException("Queue is empty");
 
+		Integer position = getPosition(procLabel);
+		int newArrayLength = array.length - 1;
+		Proc tempArray[] = new Proc[newArrayLength];
 
-    @Override
-    public int succeedingProcessTime(String procLabel) {
-     	
-    	int proceedingRunTime = 0;
-        Integer position = this.getPosition(procLabel);
-    	
-    	if(position != null) {    		
-	    	for(int i=position+1; i < array.length; i++) {
-	    		Proc currProc = array[i];
-	    		proceedingRunTime = proceedingRunTime + currProc.getRunTime();
-	    	}
-    	} else
-    		proceedingRunTime = -1;
-    		
-    	return proceedingRunTime;
-    } // end of precedingProcessTime()
+		if (position == null) {
+			return false;
+		}
 
+		for (int i = 0; i < position; i++) {
+			tempArray[i] = array[i];
+		}
 
-    @Override
-    public void printAllProcesses(PrintWriter os) {
- 
-    	String processes = "";
-    	
-    	for(int i=0; i < array.length; i++) {
-    		Proc currProc = array[i];
-    		processes = processes.concat(currProc.getProcLabel() + " ");
-    	}
-    	os.println(processes.trim());
+		for (int j = position + 1; j < array.length; j++) {
+			tempArray[j - 1] = array[j];
+		}
 
-    } // end of printAllProcesses()
-    
-    
-    public Integer getPosition(String procLabel) {
-    	
-    	Integer position = null;
-        
-    	for(int i=0; i < array.length; i++) {
-    		Proc currProc = array[i];
-    		if(procLabel.equalsIgnoreCase(currProc.getProcLabel())) {
-    			position = i;
-    			break;
-    		}
-    	}
-    	
-    	return position;
-    }
-    
-    public Integer getPosition(int vt) {
-    	
-    	Integer position = 0;
-        
-    	for(int i = array.length-1; i >= 0; i--) {
-    		Proc currProc = array[i];
-        	if(vt >= currProc.getRunTime()) {
-        		position = i+1;
-        		break;
-        	}
-    	}
-        		   		
-    	return position;
-    }
+		if (newArrayLength == 0) {
+			array = null;
+		} else {
+			array = tempArray;
+		}
+
+		return true;
+	} // end of removeProcess()
+
+	@Override
+	public int precedingProcessTime(String procLabel) {
+		int preceedingRunTime = 0;
+		Integer position = this.getPosition(procLabel);
+
+		if (position != null) {
+			for (int i = 0; i < position; i++) {
+				Proc currProc = array[i];
+				preceedingRunTime = preceedingRunTime + currProc.getRunTime();
+			}
+		} else
+			preceedingRunTime = -1;
+
+		return preceedingRunTime;
+	}// end of precedingProcessTime()
+
+	@Override
+	public int succeedingProcessTime(String procLabel) {
+		int proceedingRunTime = 0;
+		Integer position = this.getPosition(procLabel);
+
+		if (position != null) {
+			for (int i = position + 1; i < array.length; i++) {
+				Proc currProc = array[i];
+				proceedingRunTime = proceedingRunTime + currProc.getRunTime();
+			}
+		} else
+			proceedingRunTime = -1;
+
+		return proceedingRunTime;
+	} // end of precedingProcessTime()
+
+	@Override
+	public void printAllProcesses(PrintWriter os) {
+		for (int i = 0; i < array.length; i++) {
+			os.print(array[i].getProcLabel() + " ");
+		}
+		os.print("\n");
+	} // end of printAllProcesses()
+
+	public Integer getPosition(String procLabel) {
+		for (int i = 0; i < array.length; i++) {
+			if (procLabel.equalsIgnoreCase(array[i].getProcLabel())) {
+				return i;
+			}
+		}
+
+		return null;
+	}
+
+	public Integer getPosition(int vt) {
+		for (int i = 0; i < array.length; i++) {
+			if (vt < array[i].getRunTime()) {
+				return i;
+			}
+		}
+
+		return array.length;
+	}
+
+	private class Proc {
+		/** Process Label. */
+		private String mProcLabel;
+
+		/** Process RunTime. */
+		private int vt;
+
+		public Proc(String procLabel, int vt) {
+			this.mProcLabel = procLabel;
+			this.vt = vt;
+		}
+
+		public String getProcLabel() {
+			return mProcLabel;
+		}
+
+		public int getRunTime() {
+			return vt;
+		}
+	}
 
 } // end of class OrderedArrayRQ
